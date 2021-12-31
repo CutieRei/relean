a8db162e11fc9f9e0c9d59af1b7998c6d4a4e751
CutieRei <cutierei.py@gmail.com>
2021-12-31 17:09:30 +0700

    yes

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 from logging import getLogger
 from typing import Callable
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -25,7 +26,9 @@ class Relean(commands.Bot):
         self.logger = logger or getLogger(__name__)
 
     async def on_ready(self):
-        self.logger.info(f"Logged in as {self.user} in {len(self.guilds)} guilds")
+        self.logger.info(
+            f"Logged in as {self.user}[{self.user.id}] in {len(self.guilds)} guilds"
+        )
 
     async def close(self) -> None:
         self.logger.info("Stopping bot")
@@ -41,4 +44,32 @@ def get(logger=None) -> tuple[Relean, Callable]:
     bot = Relean("rl.", logger=logger)
     bot.load_extension("jishaku")
 
-    return bot, lambda: bot.run(token)
+    @bot.command()
+    @commands.is_owner()
+    async def restart(ctx):
+        async def _restart():
+            await bot.close()
+            with open(".state", "w") as f:
+                f.writelines([str(ctx.channel.id) + "\n", str(ctx.message.id)])
+
+        await ctx.reply("Scheduled restart")
+        asyncio.create_task(_restart())
+
+    async def start():
+        async def _check_restart():
+            await bot.wait_until_ready()
+            if not os.path.isfile(".state"):
+                return
+            with open(".state", "r+") as f:
+                channel, message = [int(i.strip()) for i in f.readlines()]
+            channel = bot.get_partial_messageable(
+                channel, type=discord.ChannelType.text
+            )
+            message = channel.get_partial_message(message)
+            os.remove(".state")
+            await message.reply("Restart success")
+
+        asyncio.create_task(_check_restart())
+        await bot.start(token)
+
+    return bot, start
--- run.py
+++ run.py
@@ -1,17 +1,30 @@
+import asyncio
+import bot
+from os.path import isfile
 from logging import getLogger, INFO
 from logs import ClickStreamHandler, ColouredFormatter
-from bot import get
+from importlib import reload
 
 
-def main():
+def main(module):
     logger = getLogger(__name__)
     handler = ClickStreamHandler()
     handler.setFormatter(ColouredFormatter("[%(levelname)s][%(filename)s] %(message)s"))
     logger.setLevel(INFO)
     logger.addHandler(handler)
-    _, run = get(logger)
-    run()
+    loop = asyncio.get_event_loop()
+    while True:
+        bot, start = module.get(logger)
+        try:
+            loop.run_until_complete(start())
+        except KeyboardInterrupt:
+            if not bot.is_closed():
+                loop.run_until_complete(bot.close())
+            if not isfile(".state"):
+                break
+            module = reload(module)
+    loop.close()
 
 
 if __name__ == "__main__":
-    main()
+    main(bot)
